731d1434b9b350f854a2acb6ba03e480c98e18d1
carlos-giachetti <carlos_giachetti@hotmail.com>
2018-03-24 12:42:44 -0300

    Add project files.

---
+++ Knowledge-Metaprogramming-Example01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knowledge_Metaprogramming_Example01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
